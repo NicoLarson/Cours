6e4b0952937091875c1a788eda0f6c76a79bfd83
Nicolas <nicolas.yang.1@gmail.com>
2020-03-30 05:27:45 -0300

    Mise à jour

---
+++ Dev/cours-js-SIO/js/script.js
@@ -0,0 +1,32 @@
+let x = 3
+
+const z = 4
+let msg = "Hello"
+console.log(msg)
+let valeurInt = 140
+valeurInt -= 40
+console.log(valeurInt)
+
+console.log(typeof valeurInt)
+console.log(typeof msg)
+
+let user, nombre, version, status
+user = "Eich"
+nombre = 2019
+version = 6.5
+status = true
+console.log(typeof user)
+console.log(typeof nombre)
+console.log(typeof version)
+console.log(typeof status)
+
+let printOut = parseFloat(version)
+console.log(printOut + nombre)
+
+let one = 1, two = 2, word = "hello"
+console.log(`${one} ${word} fin du script`)
+
+let tab = ["html", "js", "css"]
+console.table(tab)
+let tabAdd = ["PHP", "SQL", ...tab]
+console.table(tabAdd)
