bb272e22b70e10ba2dd21ce3cae5605154b20411
Nicolas Yang <nicolas.yang.1@gmail.com>
2020-09-13 21:38:53 +0200

    add cours 09/09/2020 dev

---
+++ Real_Maint_Comp_Log/Cours_9_09_2020/js/app.js
@@ -0,0 +1,17 @@
+document.aaddEventlistener("DOMContentLoaded",()=>{
+
+let target = document.querySelecor("ul")
+let machineALaver = ({
+	constructor(marque,prix,consommation, matricule, modele){
+	this.marque = marque
+	this.prix = prix
+	this.consommation = consommation
+	this.matricule = matricule
+	this.modele = model
+	},
+	
+	affichage(){
+		target.innerHTML += `<li>Marque}
+})
+
+})
